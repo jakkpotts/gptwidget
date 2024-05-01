1dd6868561f282901fd36c3d81b6756085070f6e
jakkpotts <jakkpotts@creamistry.xyz>
2024-04-30 21:59:56 -0700

    Added function to take user input and stream output

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 from openai import OpenAI
 
 client = OpenAI()
+
+
 # defaults to getting the key using os.environ.get("OPENAI_API_KEY")
 # if you saved the key under a different environment variable name, you can do something like:
 # client = OpenAI(
 #   api_key=os.environ.get("CUSTOM_ENV_NAME"),
 # )
 
+def query_gpt():
+    role = input("GPT Role: ")
+    ask = input("Ask: ")
 
-completion = client.chat.completions.create(
-    model="gpt-4-turbo",
-    messages=[
-        {"role": "system", "content": "You are an expert cybersecurity professional on a red team."},
-        {"role": "user", "content": "You've gained access to a target wifi network. What do you do next?"}
-    ]
-)
+    query = client.chat.completions.create(
+        model="gpt-4-turbo",
+        messages=[
+            {"role": "system", "content": role},
+            {"role": "user", "content": ask}
+        ],
+        stream=True
+    )
 
-print(completion.choices[0].message.content)
+    # Stream the response, end="" is to ensure output is streamed
+    # with readbility since the stream is in chunks
+    for chunk in query:
+        if chunk.choices[0].delta.content is not None:
+            print(chunk.choices[0].delta.content, end="")
+    print("\n")
+
+
+while True:
+    query_gpt()
+
+
+# Print the completed output
+# print(query.choices[0].message.content)
